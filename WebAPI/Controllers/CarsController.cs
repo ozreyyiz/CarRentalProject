34be028806ad7a3960c0cf9308bf50c36c0244a6
ozreyyiz <hcefe.1@gmail.com>
2024-01-07 22:49:35 +0300

    web api added

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,135 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+
+        ICarService _cardal;
+
+        public CarsController(ICarService carDal)
+        {
+            _cardal = carDal;
+        }
+
+        [HttpGet("getall")]
+
+        public IActionResult GetAll()
+        {
+            var result=_cardal.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getcarsbybrandid")]
+
+        public IActionResult GetCarsByBrandId(int id)
+        {
+            var result = _cardal.GetCarsByBrandId(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getcarsbycolorid")]
+
+        public IActionResult GetCarsByColorId(int id)
+        {
+            var result = _cardal.GetCarsByColorId(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getcarsbydailyprice")]
+
+        public IActionResult GetCarsByDailyPrice(decimal min, decimal max)
+        {
+            var result = _cardal.GetCarsByDailyPrice(min,max);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getcardetail")]
+
+        public IActionResult GetCarDetail()
+        {
+            var result = _cardal.GetCarDetail();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+
+        public IActionResult Add(Car car)
+        {
+            var result = _cardal.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("delete")]
+
+        public IActionResult Delete(Car car)
+        {
+            var result = _cardal.Delete(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+
+        public IActionResult Update(Car car)
+        {
+            var result = _cardal.Update(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
+
+
+
+
+//IDataResult<List<Car>> GetCarsByColorId(int id);
+
+//IDataResult<List<Car>> GetCarsByDailyPrice(decimal min, decimal max);
+
+//IDataResult<List<CarDetailDto>> GetCarDetail();
+
+//IResult Add(Car car);
+
+//IResult Update(Car car);
+
+//IResult Delete(Car car);
